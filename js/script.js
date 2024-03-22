5643708b2ff47aa591618a3ba961de4cb5c8ebce
mohammed-el-hayek <159185058+mohammed-el-hayek@users.noreply.github.com>
2024-03-22 03:19:36 +0000

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Mohammed El-hayek All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Mohammed El-hayek
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates area and perimeter of Triangle.
+ */
+function calculate() {
+  // input
+  const H = parseInt(document.getElementById('H of Triangle').value)
+  const A = parseInt(document.getElementById('A of Triangle').value)
+  const B = parseInt(document.getElementById('B of Triangle').value)
+  const C = parseInt(document.getElementById('C of Triangle').value)
+
+  // process
+  const area = H * B / 2
+  const perimeter = A + B + C
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
+  document.getElementById('perimeter').innerHTML = 'Perimeter is: ' + perimeter + ' cm'
+}
